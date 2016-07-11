43b7a2a3ef481ef79b62b1c12f0c466c8a0c6710
sebastian selka <sselka@symbiolab.de>
2016-07-11 14:13:12 +0200

    TRADE-234 added generic result converter to companion object

--- src/main/scala/microtools/ResultConverter.scala
+++ src/main/scala/microtools/ResultConverter.scala
@@ -3,6 +3,7 @@ package microtools
 import microtools.hateoas.BusinessResult
 import microtools.models.{Problem, Problems}
 import play.api.libs.json.{Json, Writes}
+import play.api.mvc.Results.Status
 import play.api.mvc.{Result, Results}
 
 trait ResultConverter[-R] {
@@ -10,7 +11,7 @@ trait ResultConverter[-R] {
 
   def onProblem(problem: Problem): Result
 
-  def onFailure(cause : Throwable): Result
+  def onFailure(cause: Throwable): Result
 }
 
 object ResultConverter {
@@ -19,22 +20,45 @@ object ResultConverter {
 
     override def onProblem(problem: Problem): Result = problem.asResult
 
-    override def onFailure(cause: Throwable): Result = Problems.INTERNAL_SERVER_ERROR.withDetails(cause.getMessage).asResult
-  }
-  
-  implicit def businessResultConverter[R <: BusinessResult] = new ResultConverter[R] {
-    override def onSuccess(result: R): Result = result.asResult
-
-    override def onProblem(problem: Problem): Result = problem.asResult
-
-    override def onFailure(cause: Throwable): Result = Problems.INTERNAL_SERVER_ERROR.withDetails(cause.getMessage).asResult
+    override def onFailure(cause: Throwable): Result =
+      Problems.INTERNAL_SERVER_ERROR.withDetails(cause.getMessage).asResult
   }
 
-  implicit def defaultConverter[R](implicit writes : Writes[R]) : ResultConverter[R] = new ResultConverter[R] {
-    override def onSuccess(result: R): Result = Results.Ok(Json.toJson(result))
+  implicit def businessResultConverter[R <: BusinessResult] =
+    new ResultConverter[R] {
+      override def onSuccess(result: R): Result = result.asResult
 
-    override def onProblem(problem: Problem): Result = problem.asResult
+      override def onProblem(problem: Problem): Result = problem.asResult
 
-    override def onFailure(cause: Throwable): Result = Problems.INTERNAL_SERVER_ERROR.withDetails(cause.getMessage).asResult
-  }
+      override def onFailure(cause: Throwable): Result =
+        Problems.INTERNAL_SERVER_ERROR.withDetails(cause.getMessage).asResult
+    }
+
+  implicit def defaultConverter[R](
+      implicit writes: Writes[R]): ResultConverter[R] =
+    new ResultConverter[R] {
+      override def onSuccess(result: R): Result =
+        Results.Ok(Json.toJson(result))
+
+      override def onProblem(problem: Problem): Result = problem.asResult
+
+      override def onFailure(cause: Throwable): Result =
+        Problems.INTERNAL_SERVER_ERROR.withDetails(cause.getMessage).asResult
+    }
+
+  def successResultConvert[R](success: R => Result) =
+    new ResultConverter[R] {
+      override def onSuccess(result: R): Result =
+        success(result)
+
+      override def onProblem(problem: Problem): Result =
+        problem.asResult
+
+      override def onFailure(cause: Throwable): Result = {
+        Results.InternalServerError
+      }
+    }
+
+  def jsonResultConverter[R: Writes](successStatus: Status = Results.Ok) =
+    successResultConvert((result: R) => successStatus(Json.toJson(result)))
 }
