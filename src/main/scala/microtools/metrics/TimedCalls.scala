4ff9357f3c3ec7350523c35a41ff25ab9e74d974
Untoldwind <landru@untoldwind.net>
2016-07-25 19:30:23 +0200

    Map wsresponse

--- src/main/scala/microtools/metrics/TimedCalls.scala
+++ src/main/scala/microtools/metrics/TimedCalls.scala
@@ -3,6 +3,7 @@ package microtools.metrics
 import java.time.Instant
 
 import com.codahale.metrics.{MetricRegistry, Timer}
+import microtools.{BusinessFailure, BusinessSuccess, BusinessTry}
 import microtools.logging.{LoggingContext, WithContextAwareLogger}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -34,5 +35,32 @@ trait TimedCalls extends WithContextAwareLogger {
     }
 
     result
+  }
+
+  def timedTry[T](callId:String)(block : => BusinessTry[T])(
+    implicit ec: ExecutionContext, ctx: LoggingContext): BusinessTry[T] = {
+    val timer = metricRegistry.timer(s"${log.name}.$callId")
+
+    timedTry(callId, timer)(block)
+  }
+
+  def timedTry[T](callId: String, timer: Timer)(block: => BusinessTry[T])(
+    implicit ec: ExecutionContext, ctx: LoggingContext): BusinessTry[T] = {
+
+    val timeCtx = timer.time()
+    val start = Instant.now()
+    val result = block
+
+    result.onComplete {
+      case Success(BusinessSuccess(_)) =>
+        val nanos = timeCtx.stop()
+        log.info(s"$callId: Success", "millis" -> (nanos / 1000000.0).toString)
+      case Success(BusinessFailure(problem)) =>
+        val nanos = timeCtx.stop()
+        log.info(s"$callId: Problem: $problem", "millis" -> (nanos / 1000000.0).toString)
+      case Failure(e) =>
+        val nanos = timeCtx.stop()
+        log.error(s"$callId: Internal error", e, "millis" -> (nanos / 1000000.0).toString)
+    }
   }
 }
