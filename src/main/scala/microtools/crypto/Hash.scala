5ef421e02921bca96e18259944773b4325f8feb5
Untoldwind <landru@untoldwind.net>
2016-08-17 09:22:29 +0200

    Add helper for Message digest

---
+++ src/main/scala/microtools/crypto/Hash.scala
@@ -0,0 +1,42 @@
+package microtools.crypto
+
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.util.Base64
+
+import akka.util.ByteString
+
+trait Hash {
+  def update(data: ByteBuffer): Hash
+
+  def update(data: ByteString): Hash = data.asByteBuffers.foldLeft(this)((hash, buffer) => hash.update(buffer))
+
+  def update(data: String): Hash =
+    update(ByteBuffer.wrap(data.getBytes(StandardCharsets.UTF_8)))
+
+  def update(data: Int): Hash = update(ByteBuffer.allocate(4).putInt(data))
+
+  def updata(data: Long): Hash = update(ByteBuffer.allocate(8).putLong(data))
+
+  def raw: Array[Byte]
+
+  def base64() = Base64.getEncoder.withoutPadding().encodeToString(raw)
+
+  def safeBase64() = Base64.getUrlEncoder.withoutPadding().encodeToString(raw)
+}
+
+object Hash {
+  class StdMessageDigest(algorithm: String) extends Hash {
+    val md = MessageDigest.getInstance(algorithm)
+
+    override def update(data: ByteBuffer) = {
+      md.update(data)
+      this
+    }
+
+    override def raw = md.digest()
+  }
+
+  def sha256(): Hash = new StdMessageDigest("SHA-256")
+}
