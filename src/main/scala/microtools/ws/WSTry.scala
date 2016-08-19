b4ecb576781b6fed940cfad66762882605178c94
Jentsch <daniel.jentsch@leanovate.de>
2016-08-19 13:20:10 +0200

    TRADE-401 handling of non json responses in WSTry

--- src/main/scala/microtools/ws/WSTry.scala
+++ src/main/scala/microtools/ws/WSTry.scala
@@ -2,14 +2,15 @@ package microtools.ws
 
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
-import microtools.BusinessTry
 import microtools.logging.{ContextAwareLogger, LoggingContext, WithContextAwareLogger}
 import microtools.models.{Problem, Problems}
+import microtools.{BusinessFailure, BusinessTry}
 import play.api.http.Status
-import play.api.libs.json.{JsSuccess, Reads}
+import play.api.libs.json.Reads
 import play.api.libs.ws.{StreamedResponse, WSResponse}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Success, Try}
 
 trait WSTry {
   def log: ContextAwareLogger
@@ -33,17 +34,7 @@ trait WSTry {
       case response if response.status < 300 =>
         BusinessTry.success(response)
       case response =>
-        response.json.validate[Problem] match {
-          case JsSuccess(problem, _) =>
-            log.error(
-                s"WS request failed with status=${response.status} problem=$problem")
-            BusinessTry.failure(problem)
-          case _ =>
-            log.error(
-                s"WS request failed with status=${response.status} body=${response.body}")
-            BusinessTry.failure(Problems.INTERNAL_SERVER_ERROR.withDetails(
-                    s"Non ok result: ${response.status}"))
-        }
+        notTheExpectedResponse("ok", response)
     })
 
   def expectOkJson[T](futureResponse: Future[WSResponse])(
@@ -54,17 +45,7 @@ trait WSTry {
       case response if response.status == Status.OK =>
         BusinessTry.validateJson[T](response.json)
       case response =>
-        response.json.validate[Problem] match {
-          case JsSuccess(problem, _) =>
-            log.error(
-                s"WS request failed with status=${response.status} problem=$problem")
-            BusinessTry.failure(problem)
-          case _ =>
-            log.error(
-                s"WS request failed with status=${response.status} body=${response.body}")
-            BusinessTry.failure(Problems.INTERNAL_SERVER_ERROR.withDetails(
-                    s"Non ok result: ${response.status}"))
-        }
+        notTheExpectedResponse("ok", response)
     })
 
   def expectCreatedJson[T](futureResponse: Future[WSResponse])(
@@ -75,18 +56,28 @@ trait WSTry {
       case response if response.status == Status.CREATED =>
         BusinessTry.validateJson[T](response.json)
       case response =>
-        response.json.validate[Problem] match {
-          case JsSuccess(problem, _) =>
-            log.error(
-                s"WS request failed with status=${response.status} problem=$problem")
-            BusinessTry.failure(problem)
-          case _ =>
-            log.error(
-                s"WS request failed with status=${response.status} body=${response.body}")
-            BusinessTry.failure(Problems.INTERNAL_SERVER_ERROR.withDetails(
-                    s"Non created result: ${response.status}"))
-        }
+        notTheExpectedResponse("created", response)
     })
+
+  private def notTheExpectedResponse(
+      expected: String,
+      response: WSResponse)(
+  implicit ec: LoggingContext): BusinessFailure[Nothing] = {
+    Try(response.json.as[Problem]) match {
+      case Success(problem) =>
+        log.error(
+            s"WS request failed with status=${response.status} problem=$problem")
+
+        BusinessFailure(problem)
+      case _ =>
+        log.error(
+            s"WS request failed with status=${response.status} body=${response.body}")
+
+        BusinessFailure(
+            Problems.INTERNAL_SERVER_ERROR.withDetails(
+                s"Non $expected result: ${response.status}"))
+    }
+  }
 }
 
-object WSTry extends WSTry with WithContextAwareLogger {}
+object WSTry extends WSTry with WithContextAwareLogger
---
+++ src/test/scala/microtools/ws/WSTrySpec.scala
@@ -0,0 +1,53 @@
+package microtools.ws
+
+import java.util.concurrent.TimeUnit
+
+import akka.util.Timeout
+import microtools.BusinessSuccess
+import microtools.logging.LoggingContext
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{MustMatchers, WordSpec}
+import play.api.libs.ws.WSResponse
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class WSTrySpec
+    extends WordSpec
+    with MockFactory
+    with MustMatchers
+    with ScalaFutures {
+
+  implicit val testContext = LoggingContext.static()
+  implicit val timeout     = Timeout(1, TimeUnit.SECONDS)
+
+  "expect Success" should {
+    "return a BuisnessTry of the ok result" in {
+      val okResponse = mock[WSResponse]
+      (okResponse.status _).expects.returning(200).anyNumberOfTimes()
+
+      val BusinessSuccess(response) = WSTry
+        .expectSuccess(Future.successful(okResponse))
+        .awaitResult
+
+      response must be(okResponse)
+    }
+
+    "return an failing BusinessTry and log the response content (also for non json content)" in {
+      val failedResponse = mock[WSResponse]
+      (failedResponse.status _).expects.returning(500).anyNumberOfTimes()
+      (failedResponse.json _).expects.throwing(new Exception(""))
+
+      noException must be thrownBy WSTry.expectSuccess(
+          Future.successful(failedResponse))
+
+      val errorMessage: String = WSTry
+        .expectSuccess(Future.successful(failedResponse))
+        .awaitResult
+        .toString
+      errorMessage.toString must include("Non ok result")
+    }
+  }
+
+}
