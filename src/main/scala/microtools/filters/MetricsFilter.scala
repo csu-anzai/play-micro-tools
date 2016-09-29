28a07ba93745dc5ec7c671f9b619cf7f956745c1
Untoldwind <landru@untoldwind.net>
2016-09-29 08:45:58 +0200

    Minor refactoring

--- src/main/scala/microtools/filters/MetricsFilter.scala
+++ src/main/scala/microtools/filters/MetricsFilter.scala
@@ -1,5 +1,7 @@
 package microtools.filters
 
+import javax.inject.Inject
+
 import akka.stream.Materializer
 import com.codahale.metrics.MetricRegistry
 import play.api.mvc.{Filter, RequestHeader, Result}
@@ -7,30 +9,28 @@ import play.api.mvc.{Filter, RequestHeader, Result}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Success
 
-class MetricsFilter(metricRegistry: MetricRegistry)(
+class MetricsFilter @Inject()(metricRegistry: MetricRegistry)(
     implicit ec: ExecutionContext,
     override implicit val mat: Materializer)
     extends Filter {
-  private val requestTimer = metricRegistry.timer("play.requests")
-  private val successMeter = metricRegistry.meter("play.requests.success")
+  private val successTimer = metricRegistry.timer("play.requests.success")
   private val clientErrorMeter =
     metricRegistry.meter("play.requests.clientError")
   private val serverErrorMeter =
     metricRegistry.meter("play.requests.serverError")
 
   override def apply(f: (RequestHeader) => Future[Result])(rh: RequestHeader) = {
-    val timeCtx = requestTimer.time()
-    val result  = f(rh)
+    val timeCtx = successTimer.time()
+    val futureResult  = f(rh)
 
-    result.onComplete {
+    futureResult.onComplete {
       case Success(result) if result.header.status < 400 =>
         timeCtx.stop()
-        successMeter.mark()
       case Success(result) if result.header.status < 500 =>
         clientErrorMeter.mark()
       case _ =>
         serverErrorMeter.mark()
     }
-    result
+    futureResult
   }
 }
