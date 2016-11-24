562fdc0b57d5aea73ed17a699ec32a04132b79aa
sebastian selka <sselka@symbiolab.de>
2016-11-24 11:33:46 +0100

    add RedirectHttpsFilter with log message of type warning

---
+++ src/main/scala/microtools/filters/RedirectHttpsFilter.scala
@@ -0,0 +1,29 @@
+package microtools.filters
+
+import javax.inject.{Inject, Named, Singleton}
+
+import akka.stream.Materializer
+import microtools.logging.WithContextAwareLogger
+import play.api.http.HeaderNames
+import play.api.mvc.{Filter, RequestHeader, Result, Results}
+
+import scala.concurrent.Future
+
+@Singleton
+class RedirectHttpsFilter @Inject()(@Named("base.url") baseUrl: String)(
+    implicit override val mat: Materializer)
+    extends Filter
+    with Results
+    with WithContextAwareLogger {
+
+  override def apply(nextFilter: RequestHeader => Future[Result])(
+      rh: RequestHeader): Future[Result] = {
+    if (rh.headers.get(HeaderNames.X_FORWARDED_PROTO).contains("http")) {
+      log.warn(
+        s"Forwarded protocol contains http. Service responds with SeeOther: $baseUrl${rh.path}?${rh.rawQueryString}")
+      Future.successful(SeeOther(baseUrl + rh.path + "?" + rh.rawQueryString))
+    } else {
+      nextFilter(rh)
+    }
+  }
+}
