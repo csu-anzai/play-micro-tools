0e94487eb88fad79becfa90bcc5b54cd153177c2
Untoldwind <landru@untoldwind.net>
2016-12-02 09:52:56 +0100

    TRADE-1003 add MetricsComponents

--- src/main/scala/microtools/metrics/InstrumentLogging.scala
+++ src/main/scala/microtools/metrics/InstrumentLogging.scala
@@ -9,11 +9,17 @@ import org.slf4j.{Logger, LoggerFactory}
 
 @Singleton
 class InstrumentLogging @Inject()(metricRegistry: MetricRegistry) {
-  val factory = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
-  val root = factory.getLogger(Logger.ROOT_LOGGER_NAME)
+  InstrumentLogging.instrument(metricRegistry)
+}
 
-  val instrumentedAppended = new InstrumentedAppender(metricRegistry)
-  instrumentedAppended.setContext(root.getLoggerContext)
-  instrumentedAppended.start()
-  root.addAppender(instrumentedAppended)
+object InstrumentLogging {
+  def instrument(metricRegistry: MetricRegistry): Unit = {
+    val factory = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+    val root = factory.getLogger(Logger.ROOT_LOGGER_NAME)
+
+    val instrumentedAppended = new InstrumentedAppender(metricRegistry)
+    instrumentedAppended.setContext(root.getLoggerContext)
+    instrumentedAppended.start()
+    root.addAppender(instrumentedAppended)
+  }
 }
---
+++ src/main/scala/microtools/metrics/MetricsComponents.scala
@@ -0,0 +1,13 @@
+package microtools.metrics
+
+import com.codahale.metrics.MetricRegistry
+
+trait MetricsComponents {
+  lazy val metricRegistry : MetricRegistry = {
+    val registry = new MetricRegistryProvider().get()
+
+    InstrumentLogging.instrument(registry)
+
+    registry
+  }
+}
