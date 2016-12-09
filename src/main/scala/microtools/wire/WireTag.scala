969ce3ad495766db22ba03285eb2554b309a17d0
sebastian selka <sselka@symbiolab.de>
2016-12-09 14:47:14 +0100

    TRADE-1061 add Duration as possible WireTag; remove old scalafmt; add new scalafmt.conf

--- src/main/scala/microtools/wire/WireTag.scala
+++ src/main/scala/microtools/wire/WireTag.scala
@@ -1,6 +1,7 @@
 package microtools.wire
 
 import java.net.URL
+import java.time.Duration
 
 import com.softwaremill.tagging._
 
@@ -12,6 +13,23 @@ object WireTag {
   private def findEnvOrSysProp(name: String): Option[String] =
     sys.props.get(name).orElse(sys.env.get(name)).filter(_.nonEmpty)
 
+  private def convertDuration(durationString: String): Duration = {
+    val durationPattern = "(\\d+)(s|m|h)".r
+
+    durationString match {
+      case durationPattern(number: String, unit: String) =>
+        unit match {
+          case "s" => Duration.ofSeconds(number.toLong)
+          case "m" => Duration.ofMinutes(number.toLong)
+          case "h" => Duration.ofHours(number.toLong)
+          case _   => throw new RuntimeException("this should not happen")
+        }
+      case _ =>
+        throw new RuntimeException(
+          s"could not convert $durationString to Duration. Expected a number followed by s (seconds), m (minutes), h (hours)")
+    }
+  }
+
   def givenString[TAG <: WireTag[String]](implicit tag: TAG): String @@ TAG =
     findEnvOrSysProp(tag.name).getOrElse(tag.default).taggedWith[TAG]
 
@@ -20,6 +38,12 @@ object WireTag {
 
   def givenInt[TAG <: WireTag[Int]](implicit tag: TAG): Int @@ TAG =
     findEnvOrSysProp(tag.name).getOrElse(tag.default).toInt.taggedWith[TAG]
+
+  def givenLong[TAG <: WireTag[Long]](implicit tag: TAG): Long @@ TAG =
+    findEnvOrSysProp(tag.name).getOrElse(tag.default).toLong.taggedWith[TAG]
+
+  def givenDuration[TAG <: WireTag[Duration]](implicit tag: TAG): Duration @@ TAG =
+    convertDuration(findEnvOrSysProp(tag.name).getOrElse(tag.default)).taggedWith[TAG]
 
   def givenUrl[TAG <: WireTag[URL]](implicit tag: TAG): URL @@ TAG =
     new URL(findEnvOrSysProp(tag.name).getOrElse(tag.default)).taggedWith[TAG]
