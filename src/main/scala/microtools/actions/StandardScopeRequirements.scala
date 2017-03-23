aca6ecff533cd45882235084273462b8764f9d97
Antonio Rodriguez <antonio.santiesteban@leanovate.de>
2017-03-23 16:20:28 +0100

    TRADE-1369: Fix the requirement strings to to single capital letters.

--- src/main/scala/microtools/actions/StandardScopeRequirements.scala
+++ src/main/scala/microtools/actions/StandardScopeRequirements.scala
@@ -1,12 +1,11 @@
 package microtools.actions
 
-
 object StandardScopeRequirements {
 
-  val read = ScopeRequirement.require("read")
+  val read = ScopeRequirement.require("R")
 
-  val write = ScopeRequirement.require("write")
+  val write = ScopeRequirement.require("R")
 
-  val self = ScopeRequirement.require("self")
+  val self = ScopeRequirement.require("S")
 
 }
