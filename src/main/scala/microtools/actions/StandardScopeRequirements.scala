747563eaf7b554e49464c307296011a274b6628b
Patrick Haun <bomgar85@googlemail.com>
2017-03-23 09:25:51 +0100

    TRADE-1369: add standardscopes for convenience

---
+++ src/main/scala/microtools/actions/StandardScopeRequirements.scala
@@ -0,0 +1,12 @@
+package microtools.actions
+
+
+object StandardScopeRequirements {
+
+  val read = ScopeRequirement.require("read")
+
+  val write = ScopeRequirement.require("write")
+
+  val self = ScopeRequirement.require("self")
+
+}
