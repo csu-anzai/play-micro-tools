c5f5f149e410cfe27d321457b752d94e93c824a3
Patrick Haun <bomgar85@googlemail.com>
2017-03-30 13:27:30 +0200

    Add flow-id to generic errors

--- src/main/scala/microtools/filters/ErrorHandler.scala
+++ src/main/scala/microtools/filters/ErrorHandler.scala
@@ -3,14 +3,13 @@ package microtools.filters
 import javax.inject.Inject
 
 import microtools.logging.WithContextAwareLogger
-import microtools.models.{Problem, Problems, RequestContext}
+import microtools.models.{ExtraHeaders, Problem, Problems, RequestContext}
 import play.api.Environment
 import play.api.http.HttpErrorHandler
 import play.api.mvc.{RequestHeader, Result}
+import scala.concurrent.{ExecutionContext, Future}
 
-import scala.concurrent.Future
-
-class ErrorHandler @Inject()(implicit env: Environment)
+class ErrorHandler @Inject()(implicit env: Environment, ec: ExecutionContext)
     extends HttpErrorHandler
     with WithContextAwareLogger {
   override def onClientError(request: RequestHeader,
@@ -18,12 +17,14 @@ class ErrorHandler @Inject()(implicit env: Environment)
                              message: String): Future[Result] = {
     implicit val requestContext = RequestContext.forRequest(request)
 
-    log.error(s"Client error $statusCode ${request.method} ${request.uri}: $message")
+    log.info(s"Client error $statusCode ${request.method} ${request.uri}: $message")
 
     val problem =
       Problem.forStatus(statusCode, "Client error").withDetails(message)
 
-    Future.successful(problem.asResult)
+    Future
+      .successful(problem.asResult)
+      .map(resultWithFlowId)
   }
 
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
@@ -32,8 +33,14 @@ class ErrorHandler @Inject()(implicit env: Environment)
     log.error(s"Internal server error ${request.method} ${request.uri}", exception)
 
     val problem =
-      Problems.INTERNAL_SERVER_ERROR.withDetails(exception.getMessage)
+      Problems.INTERNAL_SERVER_ERROR
 
-    Future.successful(problem.asResult)
+    Future
+      .successful(problem.asResult)
+      .map(resultWithFlowId)
+  }
+
+  private def resultWithFlowId(result: Result)(implicit requestContext: RequestContext): Result = {
+    result.withHeaders(ExtraHeaders.FLOW_ID_HEADER -> requestContext.flowId)
   }
 }
