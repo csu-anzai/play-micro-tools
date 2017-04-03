ffe64f4ae513340d98a74aec88629414fa1524fe
mriehl <max@flatmap.ninja>
2017-04-03 09:59:02 +0200

    [feat] add wsclient wrapper with flow id and auth support

---
+++ src/main/scala/microtools/ws/WSClientWithFlow.scala
@@ -0,0 +1,19 @@
+package microtools.ws
+
+import microtools.models.{AuthRequestContext, ExtraHeaders, RequestContext}
+import play.api.http.HeaderNames
+import play.api.libs.ws.{WSClient, WSRequest}
+
+class WSClientWithFlow(val underlying: WSClient) {
+
+  def url(rawUrl: String)(implicit ctx: RequestContext): WSRequest = {
+    underlying
+      .url(rawUrl)
+      .withHeaders(ExtraHeaders.FLOW_ID_HEADER -> ctx.flowId)
+  }
+
+  def urlWithAuthFromContext(rawUrl: String)(implicit ctx: AuthRequestContext): WSRequest = {
+    url(rawUrl)
+      .withHeaders(HeaderNames.AUTHORIZATION -> s"Bearer ${ctx.token}")
+  }
+}
