36bf879c303ae4a5c483487ab6a583604aa99ed0
Patrick Haun <bomgar85@googlemail.com>
2017-05-16 12:44:42 +0200

    TRADE-1321 [feat] Add forward proto support

--- src/main/scala/microtools/ws/WSClientWithFlow.scala
+++ src/main/scala/microtools/ws/WSClientWithFlow.scala
@@ -1,19 +1,43 @@
 package microtools.ws
 
-import microtools.models.{AuthRequestContext, ExtraHeaders, RequestContext}
+import microtools.models.{AuthRequestContext, ExtraHeaders, RequestContext, ServiceName}
 import play.api.http.HeaderNames
 import play.api.libs.ws.{WSClient, WSRequest}
 
+object ForwardProto extends Enumeration {
+  type Type = Value
+
+  val HTTP  = Value("http")
+  val HTTPS = Value("https")
+}
+
 class WSClientWithFlow(val underlying: WSClient) {
 
-  def url(rawUrl: String)(implicit ctx: RequestContext): WSRequest = {
+  def url(rawUrl: String)(implicit forwardProto: ForwardProto.Type,
+                          ctx: RequestContext): WSRequest = {
+    val url: String =
+      if (rawUrl.startsWith("https:"))
+        s"http:${rawUrl.drop(6)}"
+      else rawUrl
+
+    val headers = Seq(HeaderNames.X_FORWARDED_PROTO -> forwardProto.toString())
     underlying
-      .url(rawUrl)
+      .url(url)
       .withHeaders(ExtraHeaders.FLOW_ID_HEADER -> ctx.flowId)
   }
 
-  def urlWithAuthFromContext(rawUrl: String)(implicit ctx: AuthRequestContext): WSRequest = {
+  def urlWithAuthFromContext(rawUrl: String)(implicit forwardProto: ForwardProto.Type,
+                                             ctx: AuthRequestContext): WSRequest = {
     url(rawUrl)
       .withHeaders(HeaderNames.AUTHORIZATION -> s"Bearer ${ctx.token}")
+  }
+
+  def urlWithServiceAuth(rawUrl: String)(implicit forwardProto: ForwardProto.Type,
+                                         serviceName: ServiceName,
+                                         ctx: RequestContext): WSRequest = {
+    url(rawUrl)
+      .withHeaders(
+        ExtraHeaders.AUTH_SUBJECT_HEADER -> s"service/$serviceName"
+      )
   }
 }
