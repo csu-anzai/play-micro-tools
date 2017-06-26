a5003223344879854e17c8bb830a489c6bae81a8
Patrick Haun <bomgar85@googlemail.com>
2017-06-26 17:19:00 +0200

    [feat] allow numeric and fractional operations on AnyVals

---
+++ src/test/scala/microtools/anyops/AnyValNumericOperationsSpec.scala
@@ -0,0 +1,32 @@
+package microtools.anyops
+
+import org.scalatest.{MustMatchers, WordSpec}
+import microtools.anyops.AutoAnyValNumeric._
+import scala.math.Numeric.Implicits._
+import scala.math.Fractional.Implicits._
+
+case class Bauer(value: Int)   extends AnyVal
+case class Fuog(value: Double) extends AnyVal
+
+class AnuValNumericOperationsSpec extends WordSpec with MustMatchers {
+  "AnyVal operations spec" should {
+    "apply operators on anyval" in {
+      Bauer(2) + Bauer(3) must equal(Bauer(5))
+      Bauer(2) - Bauer(3) must equal(Bauer(-1))
+      Bauer(2) * Bauer(3) must equal(Bauer(6))
+      Fuog(10.0) / Fuog(2.0d) must equal(Fuog(5.0d))
+
+      Bauer(2).toInt must equal(2)
+      Bauer(2).toDouble must equal(2.0d)
+      Bauer(2).toFloat must equal(2.0f)
+      Bauer(2).toLong must equal(2L)
+
+      -Bauer(2) must equal(Bauer(-2))
+      Bauer(2) must be <= Bauer(3)
+      Bauer(3) must be >= Bauer(2)
+      Bauer(2) must be < Bauer(3)
+      Bauer(3) must be > Bauer(2)
+
+    }
+  }
+}
