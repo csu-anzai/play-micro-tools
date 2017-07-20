58454fa41bbc2f55fb4725272daa70000159f121
Patrick Haun <bomgar85@googlemail.com>
2017-07-20 11:09:55 +0200

    [refactor] remove unnecessary method

--- src/main/scala/microtools/actions/ScopedAction.scala
+++ src/main/scala/microtools/actions/ScopedAction.scala
@@ -14,7 +14,8 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.language.higherKinds
 
 sealed abstract class BaseScopedAction[R[_] <: AuthRequest[_], P[_] <: AuthRequest[_]](
-    scopeRequirement: ScopeRequirement)(implicit serviceName: ServiceName, ec: ExecutionContext)
+    scopeRequirement: ScopeRequirement)(implicit serviceName: ServiceName,
+                                        override val executionContext: ExecutionContext)
     extends ActionFunction[R, P] {
   def ensureRequestType[A](request: R[A]): BusinessTry[P[A]]
 
@@ -28,8 +29,6 @@ sealed abstract class BaseScopedAction[R[_] <: AuthRequest[_], P[_] <: AuthReque
       }
       .asResult
   }
-
-  override protected def executionContext: ExecutionContext = ec
 }
 
 case class ScopedAction(scopeRequirement: ScopeRequirement)(implicit serviceName: ServiceName,
