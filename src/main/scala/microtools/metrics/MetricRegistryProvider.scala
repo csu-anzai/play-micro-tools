7316d40bde567919392f6a75a06353c0d022ffb5
Jan Lisse <jan.lisse@21re.de>
2018-11-07 11:50:35 +0100

    [fix] Exclude JVM mem pool metrics containing single quotes

--- src/main/scala/microtools/metrics/MetricRegistryProvider.scala
+++ src/main/scala/microtools/metrics/MetricRegistryProvider.scala
@@ -1,6 +1,6 @@
 package microtools.metrics
 
-import javax.inject.{Provider, Singleton}
+import java.lang.management.{ManagementFactory, MemoryPoolMXBean}
 
 import com.codahale.metrics.MetricRegistry
 import com.codahale.metrics.jvm.{
@@ -8,14 +8,25 @@ import com.codahale.metrics.jvm.{
   MemoryUsageGaugeSet,
   ThreadStatesGaugeSet
 }
+import javax.inject.{Provider, Singleton}
+
+import scala.collection.JavaConverters._
 
 @Singleton
 class MetricRegistryProvider extends Provider[MetricRegistry] {
   private val registry = new MetricRegistry
 
   registry.register("jvm.gc", new GarbageCollectorMetricSet)
-  registry.register("jvm.memory", new MemoryUsageGaugeSet)
+  registry.register(
+    "jvm.memory",
+    new MemoryUsageGaugeSet(
+      ManagementFactory.getMemoryMXBean,
+      ManagementFactory.getMemoryPoolMXBeans.asScala.filterNot(poolNameWithNastyQuotes).asJava)
+  )
   registry.register("jvm.threads", new ThreadStatesGaugeSet)
 
   override def get(): MetricRegistry = registry
+
+  private def poolNameWithNastyQuotes(bean: MemoryPoolMXBean): Boolean = bean.getName.contains("'")
+
 }
